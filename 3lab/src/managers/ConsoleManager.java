8a5767f2b5b1ad78f3997a00e65e57004d2a72aa
Evaldas Visockas <vievaldas@gmail.com>
2021-11-04 16:48:19 +0300

    Add manager files. These are input managers. Console manager is the basic one it reads choice (action) numbers and prints general info, while user request manager collects data to change data in user repository.

---
+++ 3lab/src/managers/ConsoleManager.java
@@ -0,0 +1,78 @@
+package managers;
+
+import java.util.Scanner;
+
+public class ConsoleManager {
+
+    private Scanner scanner = new Scanner(System.in);
+    private UserRequestManager requestManager;
+
+    public void start() {
+        String datafilePath = readDatafilePath();
+        requestManager = new UserRequestManager(datafilePath, scanner);
+        int number = readActionNumber();
+        parseActionNumber(number);
+    }
+
+    public String readDatafilePath() {
+        System.out.println("Prašome įvesti kelią iki ir pavadinimą failo, kuriame yra (arba bus) vartotojų duomenis: ");
+        return scanner.nextLine();
+    }
+
+    public int readActionNumber() {
+        String message = """
+         Prašome įvesti atitinkamą skaičių:
+         1 - peržiūrėti esamus vartotojus.
+         2 - peržiūrėti vartotoją pagal jo ID.
+         3 - įvesti naują vartotoją.
+         4 - pakeisti vartotojo duomenis, pagal jo ID.
+         5 - ištrinti vartotoją, pagal jo ID.
+         """;
+        System.out.println(message);
+        String input;
+        Integer number;
+        while(true) {
+            input = scanner.nextLine();
+            try {
+                number = Integer.parseInt(input);
+                if(number < 1 || number > 5) {
+                    printIfInputIsInvalid(message);
+                }
+                else break;
+            } catch(NumberFormatException e) {
+                printIfInputIsInvalid(message);
+            }
+        }
+        return number;
+    }
+
+    public void parseActionNumber(int number) {
+        switch(number) {
+            case 1:
+                requestManager.printUsers();
+                break;
+            case 2:
+                requestManager.printUser();
+                break;
+            case 3:
+                requestManager.addUser();
+                break;
+            case 4:
+                requestManager.updateUser();
+                break;
+            case 5:
+                requestManager.deleteUser();
+                break;
+            default:
+                System.out.println("Kodo vykdymo logika, įvedus šį skaičių, nėra aprašyta!");
+                System.exit(0);
+                break;
+        }
+    }
+
+    private void printIfInputIsInvalid(String instructionMessage) {
+        System.out.println("Netinkama įvestis.");
+        System.out.println(instructionMessage);
+    }
+
+}
