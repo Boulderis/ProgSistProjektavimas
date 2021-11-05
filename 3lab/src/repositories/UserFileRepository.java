c17509f574977b2f457f70ddc474b3405b43e247
Evaldas Visockas <vievaldas@gmail.com>
2021-11-05 23:34:50 +0300

    Close scanning and writing resources appropriately. Also, pack scaning and writing closing, file renaming operations into one method for reusability.

--- 3lab/src/repositories/UserFileRepository.java
+++ 3lab/src/repositories/UserFileRepository.java
@@ -28,27 +28,31 @@ public class UserFileRepository implements UserRepository {
 
     public List<User> getUsers() {
         List<User> users = new ArrayList<User>();
+        Scanner scanner = null;
         try {
-            Scanner scanner = new Scanner(file);
+            scanner = new Scanner(file);
             while(scanner.hasNextLine()) {
                 String data = scanner.nextLine();
                 String[] split = data.split(String.valueOf(separator));
                 User user = parseUser(split);
                 users.add(user);
             }
-            scanner.close();
         } catch(FileNotFoundException e) {
             System.err.println("File not found!");
         } catch(IndexOutOfBoundsException e) {
             System.err.println("Incorrect data format!");
             e.printStackTrace();
         }
+        finally {
+            closeResourcesAndDeleteTemporaryFiles(scanner, null, null);
+        }
         return users;
     }
 
     public User getUser(long ID) {
+        Scanner scanner = null;
         try {
-            Scanner scanner = new Scanner(file);
+            scanner = new Scanner(file);
             while(scanner.hasNextLine()) {
                 String data = scanner.nextLine();
                 String[] split = data.split(String.valueOf(separator));
@@ -58,12 +62,14 @@ public class UserFileRepository implements UserRepository {
                     return user;
                 }
             }
-            scanner.close();
         } catch(FileNotFoundException e) {
             System.out.println("File not found!");
         } catch(IndexOutOfBoundsException | NumberFormatException e) {
             System.err.println("Incorrect data format!");
             e.printStackTrace();
+        }
+        finally {
+            closeResourcesAndDeleteTemporaryFiles(scanner, null, null);
         }
         return null;
     }
@@ -73,73 +79,76 @@ public class UserFileRepository implements UserRepository {
         user.setUserID(ID);
         UserValidationResult userValidationResult = userValidator.validateUser(user);
         userValidationResult.printValidationResult();
+        BufferedWriter writer = null;
         if(userValidationResult.success()) {
             try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-                writer.write(buildUser(ID, user) + System.getProperty("line.separator"));
-                writer.close();
+                writer = new BufferedWriter(new FileWriter(file, true));
+                writer.append(buildUser(ID, user) + System.getProperty("line.separator"));
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+            finally{
+                closeResourcesAndDeleteTemporaryFiles(null, writer, null);
             }
         }
         return userValidationResult;
     }
 
     public UserValidationResult updateUser(long ID, User user) {
+        File tempFile = createTempFile();
         UserValidationResult userValidationResult = userValidator.validateUser(user);
         userValidationResult.printValidationResult();
+        Scanner scanner = null;
+        BufferedWriter writer = null;
         if(userValidationResult.success()) {
-            File tempFile = new File(file.getParent() + "/tempFile.txt");
             try {
-                Scanner scanner = new Scanner(file);
-                BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+                scanner = new Scanner(file);
+                writer = new BufferedWriter(new FileWriter(tempFile));
                 while (scanner.hasNextLine()) {
                     String data = scanner.nextLine();
                     String[] split = data.split(String.valueOf(separator));
                     long currentID = Integer.parseInt(split[0]);
                     if (currentID == ID) {
-                        writer.write(buildUser(ID, user) + System.getProperty("line.separator"));
+                        writer.append(buildUser(ID, user) + System.getProperty("line.separator"));
                     } else {
-                        writer.write(data + System.getProperty("line.separator"));
+                        writer.append(data + System.getProperty("line.separator"));
                     }
                 }
-                writer.close();
-                scanner.close();
-                file.delete();
-                tempFile.renameTo(file);
-                file = tempFile;
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (IndexOutOfBoundsException | NumberFormatException e) {
                 System.err.println("Incorrect data format!");
                 e.printStackTrace();
             }
+            finally{
+                closeResourcesAndDeleteTemporaryFiles(scanner, writer, tempFile);
+            }
         }
         return userValidationResult;
     }
 
     public void deleteUser(long ID) {
-        File tempFile = new File(file.getParent() + "/tempFile.txt");
+        File tempFile = createTempFile();
+        Scanner scanner = null;
+        BufferedWriter writer = null;
         try {
-            Scanner scanner = new Scanner(file);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            scanner = new Scanner(file);
+            writer = new BufferedWriter(new FileWriter(tempFile));
             while(scanner.hasNextLine()) {
                 String data = scanner.nextLine();
                 String[] split = data.split(String.valueOf(separator));
                 long currentID = Integer.parseInt(split[0]);
                 if(currentID == ID) continue;
-                writer.write(data + System.getProperty("line.separator"));
+                writer.append(data + System.getProperty("line.separator"));
             }
-            writer.close();
-            scanner.close();
-            file.delete();
-            tempFile.renameTo(file);
-            file = tempFile;
         } catch (IOException e) {
             e.printStackTrace();
         } catch(IndexOutOfBoundsException | NumberFormatException e) {
             System.err.println("Incorrect data format!");
             e.printStackTrace();
+        }
+        finally {
+            closeResourcesAndDeleteTemporaryFiles(scanner, writer, tempFile);
         }
     }
 
@@ -193,6 +202,29 @@ public class UserFileRepository implements UserRepository {
     private void buildUserProperty(StringBuilder data, String property) {
         data.append(property);
         data.append(separator);
+    }
+
+    private File createTempFile() {
+        String parent = file.getParent();
+        return parent == null || parent.isEmpty() ? new File("tempFile.txt") : new File(parent + "/tempFile.txt");
+    }
+
+    private void closeResourcesAndDeleteTemporaryFiles(Scanner scanner, BufferedWriter writer, File tempFile) {
+        if(scanner != null) {
+            scanner.close();
+        }
+        if(writer != null) {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Failed to close writer!");
+                e.printStackTrace();
+            }
+        }
+        if(tempFile != null) {
+            file.delete();
+            tempFile.renameTo(file);
+        }
     }
 
 }
